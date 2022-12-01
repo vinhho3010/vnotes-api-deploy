6088bf42da3d2811e0e361ab69d248ba34dd14b1
vinhho3010 <anhvinh30102001@gmail.com>
2022-12-01 18:11:30 +0700

    change connect mongodb

--- app/ConnectDB/connectDB.js
+++ app/ConnectDB/connectDB.js
@@ -5,8 +5,8 @@ dotenv.config();
 
 const connectDB = async function () {
   try {
-    //const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.PASSWORD}@v-notes.hcm2ciy.mongodb.net/?retryWrites=true&w=majority`;
-    const uri = "mongodb://localhost:27017";
+    const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.PASSWORD}@v-notes.hcm2ciy.mongodb.net/?retryWrites=true&w=majority`;
+    //const uri = "mongodb://localhost:27017";
     await mongoose.connect(uri, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
