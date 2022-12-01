093a4862ccce890020c0215d89f00af3c03aaa1e
vinhho3010 <anhvinh30102001@gmail.com>
2022-12-01 19:45:20 +0700

    deploy commit

--- app/ConnectDB/connectDB.js
+++ app/ConnectDB/connectDB.js
@@ -5,9 +5,9 @@ dotenv.config();
 
 const connectDB = async function () {
   try {
-    //const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.PASSWORD}@v-notes.hcm2ciy.mongodb.net/?retryWrites=true&w=majority`;
-    const uri = "mongodb://localhost:27017";
-    await mongoose.connect(uri, {
+    const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.PASSWORD}@v-notes.hcm2ciy.mongodb.net/?retryWrites=true&w=majority`;
+    //const uri = "mongodb://localhost:27017";
+    await mongoose.connect(process.env.MONGODB_URI || uri, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
