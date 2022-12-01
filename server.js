5c262803e11ed604831158e2f8a9ebb505f64f6c
vinhho3010 <anhvinh30102001@gmail.com>
2022-12-01 18:53:00 +0700

    deploy commit

--- server.js
+++ server.js
@@ -17,8 +17,6 @@ app.use((req, res, next) => {
   return next(new ApiError(404, "Resource not found"));
 });
 
-
-
 // define error-handling middleware last, after other app.use() and routes calls
 app.use((err, req, res, next) => {
   return res.status(err.statusCode || 500).json({
